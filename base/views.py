662777fe366b7f23f51d39e560a27373a226b30b
PrashantSehrawat <Prashantsehrawat8860@gmail.com>
2023-06-09 12:29:43 +0530

    Second Commit
    Templates Structuring has been done

--- base/views.py
+++ base/views.py
@@ -2,8 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def home(request):
-    return HttpResponse("HOME PAGE")
 
-def room(request):
-    return HttpResponse("ROOM PAGE")
+rooms = [ 
+         {'id': 1 ,'title':'Lets learn Python'},
+         {'id': 2 ,'title':'Lets learn PHP'},
+         {'id': 3 ,'title':'Lets learn NODE JS'},
+         {'id': 4 ,'title':'Lets learn RUBY'},
+]
+
+
+
+def home(request):
+    context={'rooms':rooms}
+    return render(request ,'base/home.html',context)
+
+def room(request,pk):
+    room=None
+    for i in rooms:
+        if i['id']==int(pk):
+            room=i
+    context={'room':room}
+    return render(request ,'base/room.html',context)
