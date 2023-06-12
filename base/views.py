b66c32cc43ff7073c0ab10906aaa47b30f02ee4b
PrashantSehrawat <Prashantsehrawat8860@gmail.com>
2023-06-12 13:51:30 +0530

    WITH CREATE OPERATION

--- base/views.py
+++ base/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render 
+from django.http import HttpResponse 
+from django.shortcuts import redirect
 # Create your views here.
 from .models import *
 
+from base.form import CreateRoomForm
 # rooms = [ 
 #          {'id': 1 ,'title':'Lets learn Python'},
 #          {'id': 2 ,'title':'Lets learn PHP'},
@@ -21,3 +23,14 @@ def room(request,pk):
     rooms=Room.objects.get(id=pk)
     context={'rooms':rooms}
     return render(request ,'base/room.html',context)
+
+def CreateRoom(request):
+    form=CreateRoomForm()
+    if request.method == "POST":
+          print(request.POST)
+          form=CreateRoomForm(request.POST)
+          if form.is_valid():
+           form.save()
+           return redirect("/")
+    context={'form':form}
+    return render(request,'base/create_room.html',context)
